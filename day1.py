020f7a429f57181b5744ac2bcf12ccd91ccbc578
Talrejasaniya <113231579+Talrejasaniya@users.noreply.github.com>
2025-04-22 20:49:54 +0530

    Update and rename day1.py to hello_world.py

--- day1.py
+++
@@ -1,8 +0,0 @@
-# Read a line of input from the user and store it in the variable 'inputString'
-inputString = input()
-
-# Print a greeting message
-print('Hello, World.')
-
-# Print the user input on the next line
-print(inputString)
---
+++ hello_world.py
@@ -0,0 +1,7 @@
+# Day 1: Print "Hello, World." and the user's input string
+
+inputString = input()
+
+print('Hello, World.')
+print(inputString)
+
