587f017f25c10211fea659fb05ea49577c2c0694
Talrejasaniya <113231579+Talrejasaniya@users.noreply.github.com>
2025-04-22 20:44:21 +0530

    Create variables_and_input.py

---
+++ variables_and_input.py
@@ -0,0 +1,19 @@
+# Day 2: Read and process integer, float, and string input, then print their sums and concatenation
+
+i1 = int(input())
+d1 = float(input())
+s1 = str(input())
+
+# Predefined variables
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Print the sum of integers
+print(i + i1)
+
+# Print the sum of doubles
+print(round(d + d1, 1))
+
+# Print the concatenated strings
+print(s + s1)
