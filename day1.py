9106a6a9644d91e98abe89c9536e36552f4bf281
Talrejasaniya <113231579+Talrejasaniya@users.noreply.github.com>
2025-04-22 20:37:42 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,8 @@
+# Read a line of input from the user and store it in the variable 'inputString'
+inputString = input()
+
+# Print a greeting message
+print('Hello, World.')
+
+# Print the user input on the next line
+print(inputString)
